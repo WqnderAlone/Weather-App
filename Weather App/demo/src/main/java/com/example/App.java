53c5489c2a46e02641247f402dc82a18c2801a69
WqnderAlone <69101440+WqnderAlone@users.noreply.github.com>
2022-05-30 18:13:05 -0700

    Update App.java

--- Weather App/demo/src/main/java/com/example/App.java
+++ Weather App/demo/src/main/java/com/example/App.java
@@ -4,7 +4,7 @@ public class App
 {
     public static void main( String[] args ) throws Exception
     {
-        String key = "92ec8c9b181a1a4446541e4282ddadb2";
+        String key = "";
         new GUI(key);
     }
 }
